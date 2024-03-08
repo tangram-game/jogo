1ea5d8110c539f19a02603ef16c5902ca12e79d5
RamosRrG <161327930+RamosRrG@users.noreply.github.com>
2024-03-08 12:09:04 +0000

    criar servidor mínimo com express

---
+++ cliente/config.js
@@ -0,0 +1,21 @@
+
+export default {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 450,
+  parent: 'game-container',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'game-container',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 450
+  }
+}
--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 450,
-  parent: 'game-container'
-}
