bebfd856c0ead12ac12dff11c972921d7330215f
RamosRrG <161327930+RamosRrG@users.noreply.github.com>
2024-04-02 12:19:11 +0000

    Criar vetor de palavras

---
+++ estrutora_sorteio_partida.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('div[id^=num]', 'table').click(function() {
+      var id = $(this).attr("id");
+    });
+  });
+  var palavras = ['casa', 'maçã']
+  palavras[0] = "casa";
+  palavras[1] = "Maça";
+  palavras[2] = "Diamante";
+  palavras[3] = "Coração";
+  palavras[4] = "Árvore(Pinheiro)";
+  palavras[5] = "Espada";
+  palavras[6] = "Árvore";
+  palavras[7] = "Flor";
+  palavras[8] = "Foguete";
+  palavras[9] = "Torre";
+  palavras[10] = "Camelo";
+  palavras[11] = "Rinoceronte";
+  palavras[12] = "Urso";
+  palavras[13] = "Tartaruga";
+  palavras[14] = "Coelho";
+  palavras[15] = "Avião";
+  palavras[16] = "Carro";
+  palavras[17] = "Bicicleta";
+  palavras[18] = "Barco";
+  palavras[19] = "Golfinho";
+  palavras[20] = "Cabrito";
+  palavras[21] = "Galinha";
+  palavras[22] = "Cisnei";
+  palavras[23] = "Hipopotamo";
+  palavras[24] = "Gato";
+  palavras[25] = "Leão";
+  palavras[26] = "Tubarão";
+  palavras[27] = "Peixe";
+  palavras[28] = "Jacaré";
+  palavras[29] = "Águia ";
+  palavras[30] = "Papagaio";
+  palavras[31] = "Piriquito";
+  palavras[32] = "Cachorro";
+  palavras[33] = "Vaca";
+  palavras[34] = "Cavalo";
+ "Baleia";
+
+ 
