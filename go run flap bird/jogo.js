587863284f825e3a5a6f5e2fb30ed3993f2f88c2
RamosRrG <161327930+RamosRrG@users.noreply.github.com>
2024-07-15 22:24:44 +0000

    primeiros passos na criação do jogo

---
+++ go run flap bird/jogo.js
@@ -0,0 +1,37 @@
+
+
+const sprites = new Image();
+sprites.src = './sprits.png';
+
+
+
+const canvas = document.querySelector("canvas");
+canvas.widht = 320;
+canvas.height = 480;
+const contexto = canvas.getContext("2d");
+
+const flappybird = {
+    spritX:             //recorte da imagem importada
+    spritY:             //recort da imagem importada
+    largura:            //tamanho do sprite no jogo
+    altura:             //tamanho do sprite no jogo
+    x:                  //localização do sprite no canvas
+    y:                  //localização do sprite no canvas
+}
+
+function loop() { 
+    contexto.drawImage(
+   
+        desenha() {
+
+
+
+
+    }
+}
+
+
+
+
+}
+
